10c342c2bc6707cd073c8c2aca20035835fbc25f
Hildegar Medina <medinahildegar1@gmail.com>
2020-08-05 00:04:23 -0400

    Se agrego Bootstrap y se hizo el diseño de inicio (Nuevo usuario)

---
+++ view/new-user.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Chat Hidden - Welcome</title>
+
+    <!-- STYLES : CSS -->
+    <link rel="stylesheet" href="../view/assets/css/normalize.css">
+    <link rel="stylesheet" href="../view/assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../view/assets/css/styles.css">
+</head>
+<body>
+
+    <!-- HEADER -->
+    <header class="bg-dark">
+        <div class="container text-center p-3">
+            <span class="text-white h3">Chat hidden</span>
+        </div>
+    </header>
+
+    <!-- CONTENT -->
+    <main>
+        <div class="container">
+        
+            <form action="#" id="form-new" class="mt-4 mx-auto p-5 border rounded bg-white">
+
+                <h2>Welcome, you need create a account temporary</h2>
+            
+                <div class="form-group">
+                    <label for="nick">Nick</label>
+                    <input type="text" class="form-control" id="nick">
+                </div>
+
+                <div class="form-group">
+                    <label for="hash">Your Hash</label>
+                    <input type="text" class="form-control" id="hash">
+                </div>
+
+                <p class="">Important: your hast is used for that other user chat with you</p>
+
+                <input type="submit" class="btn btn-dark btn-block" value="Join now">
+
+            </form>
+
+        </div>
+    </main>
+
+    <!-- SCRIPTS : JS -->
+    <script src="../view/assets/js/jquery-3.4.1.min.js"></script>
+    <script src="../view/assets/js/bootstrap.min.js"></script>
+    <script src="../view/assets/js/bootstrap.bundle.min.js"></script>
+    
+</body>
+</html>
